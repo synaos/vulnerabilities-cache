5f4676bf5984f0a62c7ba80386f09e983d1ce5e1
Gregor Noczinski <gregor@noczinski.eu>
2024-10-25 22:16:56 +0200

    Added path.

--- java/core/src/main/java/com/github/synaos/vulnerabilitiescache/dependencies/Owner.java
+++ java/core/src/main/java/com/github/synaos/vulnerabilitiescache/dependencies/Owner.java
@@ -1,12 +1,17 @@
 package com.github.synaos.vulnerabilitiescache.dependencies;
 
+import java.nio.file.Path;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 
 import com.github.synaos.vulnerabilitiescache.packages.VersionedPackageRef;
 
-public interface Owner {
+public interface Owner extends Dependencies {
 
     @Nonnull
     VersionedPackageRef ref();
+
+    @Nonnull
+    Optional<Path> path();
 
 }
